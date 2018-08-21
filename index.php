c07c14fe9026fb800e0a41f8aea409ed05afa65d
Developer <desenvolvimento@fazendomais.com.br>
2018-08-21 17:41:32 -0300

    Adicionado arquivo script e index

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
 ?>
 
 <!doctype html>
-<html lang="pt-br">
+<html lang="pt-br" ng-app="pessoas">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>SON Angular JS</title>
+    <title>Sample AngularJS Pessoas</title>
 
     <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap-reboot.min.css"/>
     <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css"/>
@@ -18,60 +18,21 @@
 
 
 </head>
-<body ng-app="myApp">
-<!--<div ng-app="myApp" ng-controller="myCtrl">
-    <input type='text' ng-model='name' ng-change='change()'>
-    <br/> <span>changed {{counter}} times </span>
-</div>-->
-<div class="container" ng-controller="CtrlApp">
+<body>
+
+<div class="container" ng-controller="CtrlPessoas">
 
     <div class="row text-center">
         <div class="col"><h1 class="text-success"> {{nome}}</h1></div>
     </div>
 
-    <div class="row">
-        <div class="col-3">
-            <label for="nome" class="control-label">Filtro</label>
-            <input type="text"  name="nome" id="nome" ng-model="buscar" ng-change="logar()" class="form-control" /></div>
-    </div>
-    <div class="row col" ng-controller="CtrlListaPerfis">
-        <div class="col-12">
-            Total: <div class="badge badge-info font-weight-bold">{{perfis.length}}</div>
-        </div>
-
-        <div class="col-12">
-            <select name="selected" id="selected" class="col-3 form-control"
-                    ng-model="selected"
-                    ng-options="perfil.status for perfil in perfis | filter:buscar">
-                <option value="">Selecione um status</option>
-            </select>
-        </div>
-
-        <div class="col">
-            <ul>
-                <li ng-repeat="perfil in perfis">{{perfil.nome}}
-                    <btn ng-click="edit($index)" class="btn btn-outline-info fa fa-edit"></btn>
-                    <btn ng-click="delete($index)" class="btn btn-outline-danger fa fa-trash"></btn>
-                </li>
-            </ul>
-
-        </div>
-
-        <div class="col">
-            <label for="perfil" class="control-label">Perfil</label>
-            <input type="text" name="perfil" id="perfil" ng-model="vPerfil" class="form-control" />
-            <label for="status" class="control-label">Status</label>
-            <input type="text" name="status" id="status" ng-model="vStatus" class="form-control" />
-            <div class="col mt-2">
-                <btn ng-click="add()" class="btn btn-success">Click para adicionar</btn>
-
-            </div>
-
-        </div>
+    <ul>
+        <li ng-repeat="pessoa in pessoas"> {{pessoa.nome}}, {{pessoa.cidade}}</li>
 
 
+    </ul>
 
-    </div>
+
 
 
 </div>
@@ -79,18 +40,11 @@
 <script src="bower_components/jquery/dist/jquery.min.js"></script>
 <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 <script src="bower_components/angular/angular.min.js"></script>
-<script src="script.js"></script>
+<script src="sample_ng_app_base/js/script.js"></script>
 
 <script>
 
-    var app = angular.module('myApp', []);
-    app.controller('myCtrl', function($scope) {
-        $scope.name = 'Australia';
-        $scope.counter = 0;
-        $scope.change = function() {
-            $scope.counter++;
-        };
-    });
+
 </script>
 </body>
 </html>
--- sample_ng_app_base/js/script.js
+++ sample_ng_app_base/js/script.js
@@ -1,59 +1,15 @@
-var CtrlApp = function ($scope) {
+var app = angular
+    .module('pessoas', [])
+    .controller('CtrlPessoas', ['$scope', function (s) {
 
-    $scope.nome = "Developer Web with AngularJS 1x"
-    $scope.logar = function() {
-        console.log('teste:', $scope.buscar);
-    };
-
-};
-var app = angular.module('myApp', []);
-var CtrlListaPerfis  = function ($scope) {
-
-    $scope.perfis = [
-        {nome:  'Administrador de Sistemas', status: 'Ativo'},
-        {nome:  'Analista de Sistemas', status: 'Ativo'},
-        {nome:  'Developer Front End', status: 'Ativo'},
-        {nome:  'Developer Back End', status: 'Ativo'},
-        {nome:  'Developer Web', status: 'Ativo'},
-        {nome:  'Developer Full Stack', status: 'Ativo'},
-        {nome:  'Suporte Técnico Nível 1', status: 'Bloqueado'},
-        {nome:  'Suporte Técnico Nível 2', status: 'Bloqueado'},
-        {nome:  'Suporte Técnico Nível 3', status: 'Bloqueado'},
-    ];
-
-    $.unique([2,3,4,5,5,4]);
-    /*$scope.statusOptions = $scope.perfis.map(i => i.status);*/
-
-    console.log('status:', $scope.statusOptions);
-
-    /*$scope.$watch('buscar', function () {
-   console.log($scope.buscar);
-    });*/
-
-  /*  $scope.$watch('buscar', function () {
-        console.log($scope.buscar);
-    });*/
-    
-    $scope.add = function () {
-        $scope.perfis.push({
-            nome: $scope.vPerfil,
-            status: $scope.vStatus,
-        });
-
-        $scope.vPerfil = '';
-        $scope.vStatus = '';
-    };
-
-    $scope.delete = function (index) {
-        $scope.perfis.splice(index,1);
-    };
-
-    $scope.edit = function (index) {
-      console.log($scope.perfis[index]);
-      $scope.vPerfil = $scope.perfis[index].nome;
-      $scope.vStatus = $scope.perfis[index].status;
-
-    };
-
-
-}
+        s.nome  = 'Listagem de Pessoas';
+        s.pessoas = [
+            {nome: 'Teste 1', cidade: 'DF'},
+            {nome: 'Teste 2', cidade: 'BA'},
+            {nome: 'Teste 3', cidade: 'SP'},
+            {nome: 'Teste 4', cidade: 'MG'},
+            {nome: 'Teste 5', cidade: 'AC'},
+            {nome: 'Teste 6', cidade: 'BH'},
+            {nome: 'Teste 7', cidade: 'BA'}
+        ];
+    }]);
